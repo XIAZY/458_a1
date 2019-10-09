9dafe50842920de99a3e382dbe6655ef2eb16475
Chen Liang <c.liang@mail.utoronto.ca>
2019-10-09 12:08:18 -0400

    THIS IS FIRST CHANGE FOR ROUTER

--- router/sr_router.c
+++ router/sr_router.c
@@ -80,5 +80,140 @@ void sr_handlepacket(struct sr_instance* sr,
 
   /* fill in code here */
 
-}/* end sr_ForwardPacket */
+  switch(ethertype(packet)){
+    case ethertype_ip: {
+      printf("An IP Packet was received\n");
+      handle_ip_packet(sr, packet, len, interface);
+      break;
+    }
+    case ethertype_arp: {
+      printf("An ARP Packet was received\n");
+      handle_arp_packet(sr, packet, len, interface);
+      break;
+    }
+  }
+
+}
+
+void sr_handle_ip_packet(struct sr_instance* sr,
+        uint8_t * packet/* lent */,
+        unsigned int len,
+        char* interface/* lent */)
+{
+  /* REQUIRES */
+  assert(sr);
+  assert(packet);
+  assert(interface);
+
+  struct sr_arpcache *sr_cache = &sr->cache;
+
+  printf("*** -> Received IP packet of length %d \n",len);
+
+  sr_ip_hdr_t *ip_hdr = (sr_ip_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t));
+
+  /* Check whether the length reach the minimum length requirment. */
+  if (len < sizeof (sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t)) {
+    perror("Error: The header length does not satisfy the minimum requirement");
+  }
+
+  /* Check whether the checksum are the same as requested. */
+  uint16_t received_checksum = ip_hdr->ip_sum;
+  ip_hdr->ip_sum = 0;
+  uint16_t actual_checksum = cksum(ip_hdr, ip_hdr->ip_hl * 4);
+  ip_hdr->ip_sum = received_checksum;
+  if(received_checksum != actual_checksum) {
+    perror("Error: The received checksum does not equal to the actual one.");
+  }
+
+  struct sr_if *target_iface = get_router_interface (ip_hdr->ip_dst, sr);
+
+  /* If the IP packet is for interfaces of the router*/
+  if (target_iface) {
+    printf("The packet is for one of the interfaces of this router\n");
+    uint8_t ip_p = ip_protocol((uint8_t *)ip_hdr->ip_p); 
+    switch(ip_p) {
+      /* If the ip protocol is ICMP*/
+      case ip_protocol_icmp: {
+        printf("Packet is a ICMP echo request.\n");
+              
+        /* Check length reach the minimum length requirement of ICMP */
+        if(len < sizeof(sr_ethernet_hdr_t) + sizeof(sr_icmp_hdr_t) + (ip_hdr->ip_hl * 4)) {
+          perror("Error: The header length does not satisfy the minimum requirement");
+        }
+
+
+        /* Checksum for ICMP. */
+        sr_icmp_hdr_t* icmp_hdr = (sr_icmp_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
+        uint16_t received_checksum = icmp_hdr->icmp_sum;
+        uint16_t actual_checksum = cksum(icmp_hdr, ntohs(ip_hdr->ip_len) - (ip_hdr->ip_hl * 4));
+        icmp_hdr->icmp_sum = received_checksum;
+        if(received_checksum != actual_checksum) {
+          perror("Error: The received checksum does not equal to the actual one.");
+        }
+
+        /* If it's ICMP echo req, send echo reply */
+        if (icmp_hdr->icmp_type == icmp_echo_request) {
+          send_echo_reply(sr, packet, len, interface);
+        } else {
+          printf("The type of ICMP packet is unknown.\n");
+        }
+
+      /* If it's TCP / UDP, send ICMP port unreachable */
+      } 
+      case ip_protocol_tcp:
+      case ip_protocol_udp: {
+        printf("Packet is a TCP/UDP message.\n");
+        send_icmp_port_unreachable(sr, packet, len, interface);
+      }
+    }
+  /* If the IP Packet is not for interfaces of the router. */
+  } else {
+    printf("If the IP Packet is not for interfaces of the router.\n");
+
+    /* check routing table, and perform LPM */ 
+    struct sr_rt* table_entry = get_longest_matching_prefix(sr, ip_hdr->ip_dst);
+    if (table_entry) {
+        /* find routing table indicated interface */
+        struct sr_if* rt_out_interface = sr_get_interface(sr, table_entry->interface);
+        /* check ARP cache */
+        struct sr_arpentry * arp_entry = sr_arpcache_lookup (sr_cache, table_entry->gw.s_addr); 
+        /* send frame to next hop*/
+        if (arp_entry) {
+            printf("There is a match in the ARP cache\n");
+        }
+    } else {
+      
+    }
+
+  }
+}
+
+
+void send_echo_reply (struct sr_instance* sr, uint8_t * packet, unsigned int len, char* interface) {
+}
+
+void send_icmp_port_unreachable (struct sr_instance* sr, uint8_t * packet, unsigned int len, char* interface) {
+
+}
+
+struct sr_rt * routing_lpm (struct sr_instance* sr, uint32_t ip_dst) {
+    struct sr_rt *routing_table = sr->routing_table;
+    int len = 0; 
+    struct sr_rt* rt_walker = 0;
+    struct sr_rt* longest_prefix = 0;
+    rt_walker = routing_table;
+    while (rt_walker) {
+        /* Compare the bitwise AND of target and the subnet mask with the bitwise AND of routing table entry and the subnet mask */
+        if ((ip_dst & rt_walker->mask.s_addr) == (rt_walker->dest.s_addr & rt_walker->mask.s_addr)) {
+            if ((ip_dst & rt_walker->mask.s_addr) > len){
+                len = ip_dst & rt_walker->mask.s_addr;
+                longest_prefix = rt_walker;
+            }
+        }
+        rt_walker = rt_walker->next; 
+    }
+    return longest_prefix;
+}
+
+/* end sr_ForwardPacket */
 
