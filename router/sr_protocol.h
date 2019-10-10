e6f84fd646e9d5886a220f951314f23eacc69966
Chen Liang <c.liang@mail.utoronto.ca>
2019-10-09 23:30:40 -0400

    SLIGHT CHANGE ON PROTOCOL

--- router/sr_protocol.h
+++ router/sr_protocol.h
@@ -108,6 +108,12 @@ enum icmp_case {
   timeout = 19,
 };
 
+enum sr_ip_protocol {
+  ip_protocol_icmp = 1,
+  ip_protocol_tcp = 6,
+  ip_protocol_udp = 11,
+};
+
 enum icmp_type {
   icmp_type_echo_reply = 0,
   icmp_type_unreachable = 3,
